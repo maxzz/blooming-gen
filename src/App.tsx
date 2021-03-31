b47f7a69da770a76bf0a0315bd8a34ff8ae85600
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-30 20:31:29 -0700

    Initial SVG setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App text-2xl text-gray-100">
-            aa33
+        <div className="App h-screen bg-gray-100">
+            <div className="max-w-md mx-auto bg-indigo-100 h-full">
+                <div className="w-24 h-24 border border-dotted border-red-800">
+                    <svg className="" viewBox="-24 -24 48 48">
+                        <rect x='-10' y='-10' width='20' height='20' fill="red" />
+                    </svg>
+                </div>
+            </div>
         </div>
     );
 }
