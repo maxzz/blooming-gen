39d3e1ffb7c0a6e5e9664d916054327e81ec0583
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-02 02:20:43 -0700

    Feature: code clean up

--- src/components/SimpleCurve.tsx
+++ src/components/SimpleCurve.tsx
@@ -54,49 +54,24 @@ function PointLines({points}: {points: PathPoint[]}) {
     }
 }
 
+function makePath(points: PathPoint[]): string {
+    return points.map(point => `${point.c} ${point.d?.map(xy => `${xy.x} ${xy.y}`) || ''}`).join(' ');
+}
+
 function SimpleCurve() {
-    const radius = 3;
-    const point1 = {
-        x: 0,
-        y: 0,
-    };
-    const point2 = {
-        x: 180,
-        y: 0,
-    };
-    const pointc = {
-        x: 100,
-        y: -160,
-    };
 
     let pathPoints: PathPoint[] = [
         { c: 'M', d: [{ x: 0, y: 0 },] },
         { c: 'Q', d: [{ x: 100, y: -160 }, { x: 180, y: 0 },] },
     ];
-    function makePath(points: PathPoint[]): string {
-        return points.map(point => `${point.c} ${point.d?.map(xy => `${xy.x} ${xy.y}`) || ''}`).join(' ');
-    }
-
-
 
     return (
         <div className="max-w-md mx-auto bg-indigo-100 h-full">
             <div className="w-96 h-96 border border-dotted border-red-800">
                 <svg className="bg-red-100" viewBox="-200 -200 400 400">
                     <SvgGrid x={-200} y={-200}/>
-
-                    {/* <circle cx={point1.x} cy={point1.y} r={radius} fill='red' />
-                    <circle cx={point2.x} cy={point2.y} r={radius} fill='blue' />
-                    <circle cx={pointc.x} cy={pointc.y} r={radius} fill='lime' /> */}
-
                     <PointMarkers points={pathPoints} />
-
-                    {/* <line x1={point1.x} y1={point1.y} x2={pointc.x} y2={pointc.y} stroke="red" />
-                    <line x1={pointc.x} y1={pointc.y} x2={point2.x} y2={point2.y} stroke="blue" /> */}
-
                     <PointLines points={pathPoints} />
-
-                    {/* <path d={`M ${point1.x} ${point1.y} Q ${pointc.x} ${pointc.y} ${point2.x} ${point2.y}`} stroke="black" fill="transparent" /> */}
                     <path d={`${makePath(pathPoints)}`} stroke="black" fill="transparent" />
                 </svg>
             </div>
