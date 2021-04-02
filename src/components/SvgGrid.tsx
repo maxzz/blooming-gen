a3bafa8df6fc21f6dbd93148ce165344e6002289
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-01 20:02:54 -0700

    Fix: cannot hide original SVG

---
+++ src/components/SvgGrid.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SvgGrid = () => {
+    return (
+        <svg width="100%" height="100%" x="-200" y="-200">
+            <defs>
+                <pattern id="smallGrid" width="5" height="5" patternUnits="userSpaceOnUse">
+                    <path d="M 10 0 L 0 0 0 10" fill="none" stroke="#80808020" strokeWidth=".5" />
+                </pattern>
+                <pattern id="medSmallGrid" width="10" height="10" patternUnits="userSpaceOnUse">
+                    <rect width="100" height="100" fill="url(#smallGrid)" />
+                    <path d="M 10 0 L 0 0 0 10" fill="none" stroke="#80808040" strokeWidth=".7" />
+                </pattern>
+                <pattern id="largeGrid" width="100" height="100" patternUnits="userSpaceOnUse">
+                    <rect width="100" height="100" fill="url(#medSmallGrid)" />
+                    <path d="M 100 0 L 0 0 0 100" fill="none" stroke="#ff000040" strokeWidth="1" />
+                </pattern>
+            </defs>
+            <rect width="100%" height="100%" fill="url(#largeGrid)" />
+        </svg>
+    );
+};
+
+export default SvgGrid;
