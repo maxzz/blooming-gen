739c78b6ba7b5d5d92a6f2e6d41ee94965bac591
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-03 18:29:53 -0700

    Feature: WIP parse d path attribute

--- src/components/SimpleCurve.tsx
+++ src/components/SimpleCurve.tsx
@@ -107,6 +107,68 @@ function MarkPathPoints({ pathPoints, ...rest }: { pathPoints: PathPoint[]; } & 
     </>);
 }
 
+function parsePathString(pathString: string) {
+    if (!pathString) {
+        return null;
+    }
+
+    console.log('src', pathString);
+    
+    let paramCounts = {a: 7, c: 6, o: 2, h: 1, l: 2, m: 2, r: 4, q: 4, s: 4, t: 2, v: 1, u: 3, z: 0};
+    let data: any[] = [];
+
+    const spaces = "\x09\x0a\x0b\x0c\x0d\x20\xa0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u2028\u2029";
+    const pathCommand = new RegExp("([a-z])[" + spaces + ",]*((-?\\d*\\.?\\d*(?:e[\\-+]?\\d+)?[" + spaces + "]*,?[" + spaces + "]*)+)", "ig");
+    const tCommand = new RegExp("([rstm])[" + spaces + ",]*((-?\\d*\\.?\\d*(?:e[\\-+]?\\d+)?[" + spaces + "]*,?[" + spaces + "]*)+)", "ig");
+    const pathValues = new RegExp("(-?\\d*\\.?\\d*(?:e[\\-+]?\\d+)?)[" + spaces + "]*,?[" + spaces + "]*", "ig");
+
+    pathString.replace(pathCommand, 
+        function (a: string, b: string, c: string) {
+            let params: any[] = [];
+            let name = b.toLowerCase() as keyof typeof paramCounts;
+
+            c.replace(pathValues, function (a, b) {
+                b && params.push(+b);
+                return b; // temp
+            });
+
+            if (name == "m" && params.length > 2) {
+                data.push([b].concat(params.splice(0, 2)));
+                name = "l";
+                b = b == "m" ? "l" : "L";
+            }
+
+            if (name == "o" && params.length == 1) {
+                data.push([b, params[0]]);
+            }
+
+            if (name == "r") {
+                data.push([b].concat(params));
+            } else {
+                while (params.length >= paramCounts[name]) {
+                    data.push([b].concat(params.splice(0, paramCounts[name])));
+                    if (!paramCounts[name]) {
+                        break;
+                    }
+                }
+            }
+
+            return b; // temp
+        }
+    );
+
+    console.log('data', data);
+}
+
+parsePathString("M18,69.48s-.6-11.27-3-30.86S30.43.34,30.43.34");
+/*
+'src M18,69.48s-.6-11.27-3-30.86S30.43.34,30.43.34'
+(3) [Array(3), Array(5), Array(5)]
+    0: (3) ["M", 18, 69.48]
+    1: (5) ["s", -0.6, -11.27, -3, -30.86]
+    2: (5) ["S", 30.43, 0.34, 30.43, 0.34]
+*/
+
 function SimpleCurve() {
 
     let pathPoints: PathPoint[] = PathPoints.generateCurve({ start: { x: -200, y: -100 }, end: { x: 200, y: -150 }, steps: 4 });
@@ -129,16 +191,11 @@ function SimpleCurve() {
                 </svg>
             </div>
 
-            <div className="mx-6 my-2" >
-
+            <div className="ml-4 py-2" >
                 <svg className="w-16 h-16" viewBox="0 0 33.84 69.68" fill="none" stroke="green">
-                            <path d="M18,69.48S33.7,60,33.7,49s-4.46-16.32-6.24-24.63,3-24,3-24A142.07,142.07,0,0,0,14.11,12.8C7.71,18.56.76,25.27.16,36.84S18,69.48,18,69.48Z"/>
-                            <path d="M18,69.48s-.6-11.27-3-30.86S30.43.34,30.43.34"/>
+                    <path d="M18,69.48S33.7,60,33.7,49s-4.46-16.32-6.24-24.63,3-24,3-24A142.07,142.07,0,0,0,14.11,12.8C7.71,18.56.76,25.27.16,36.84S18,69.48,18,69.48Z"/>
+                    <path d="M18,69.48s-.6-11.27-3-30.86S30.43.34,30.43.34"/>
                 </svg>                
-                <svg className="w-32 h-32" viewBox="0 0 1366 768" fill="none" stroke="green">
-                    <path className="st0" d="M42 85s16-10 16-21-4-16-6-25 3-24 3-24L38 28c-6 6-13 12-13 24-1 11 17 33 17 33z" />
-                    <path className="st0" d="M42 85s0-12-3-31c-2-20 16-39 16-39" />
-                </svg>
             </div>
         </div>
     );
