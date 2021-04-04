7bd49b64785c7234d064072dddc159abfda3dd0a
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-03 18:46:14 -0700

    Feature: WIP parse d path attribute. 2

--- src/components/SimpleCurve.tsx
+++ src/components/SimpleCurve.tsx
@@ -107,6 +107,8 @@ function MarkPathPoints({ pathPoints, ...rest }: { pathPoints: PathPoint[]; } & 
     </>);
 }
 
+type SvgTuple = [string, number?, number?, number?, number?, number?, number?];
+
 function parsePathString(pathString: string) {
     if (!pathString) {
         return null;
@@ -117,20 +119,18 @@ function parsePathString(pathString: string) {
     let paramCounts = {a: 7, c: 6, o: 2, h: 1, l: 2, m: 2, r: 4, q: 4, s: 4, t: 2, v: 1, u: 3, z: 0};
     let data: any[] = [];
 
-    const spaces = "\x09\x0a\x0b\x0c\x0d\x20\xa0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u2028\u2029";
-    const pathCommand = new RegExp("([a-z])[" + spaces + ",]*((-?\\d*\\.?\\d*(?:e[\\-+]?\\d+)?[" + spaces + "]*,?[" + spaces + "]*)+)", "ig");
-    const tCommand = new RegExp("([rstm])[" + spaces + ",]*((-?\\d*\\.?\\d*(?:e[\\-+]?\\d+)?[" + spaces + "]*,?[" + spaces + "]*)+)", "ig");
-    const pathValues = new RegExp("(-?\\d*\\.?\\d*(?:e[\\-+]?\\d+)?)[" + spaces + "]*,?[" + spaces + "]*", "ig");
+    const reSpaces = "\x09\x0a\x0b\x0c\x0d\x20\xa0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u2028\u2029";
+    const rePathCommand = new RegExp(`([a-z])[${reSpaces},]*((-?\\d*\\.?\\d*(?:e[\\-+]?\\d+)?[${reSpaces}]*,?[${reSpaces}]*)+)`, "ig");
+    const rePathValues = new RegExp(`(-?\\d*\\.?\\d*(?:e[\\-+]?\\d+)?)[${reSpaces}]*,?[${reSpaces}]*`, "ig");
 
-    pathString.replace(pathCommand, 
+    pathString.replace(rePathCommand, 
         function (a: string, b: string, c: string) {
             let params: any[] = [];
             let name = b.toLowerCase() as keyof typeof paramCounts;
 
-            c.replace(pathValues, function (a, b) {
+            c.replace(rePathValues, function (a: string, b: string) {
                 b && params.push(+b);
-                return b; // temp
-            });
+            } as any);
 
             if (name == "m" && params.length > 2) {
                 data.push([b].concat(params.splice(0, 2)));
@@ -152,9 +152,7 @@ function parsePathString(pathString: string) {
                     }
                 }
             }
-
-            return b; // temp
-        }
+        } as any
     );
 
     console.log('data', data);
@@ -162,11 +160,11 @@ function parsePathString(pathString: string) {
 
 parsePathString("M18,69.48s-.6-11.27-3-30.86S30.43.34,30.43.34");
 /*
-'src M18,69.48s-.6-11.27-3-30.86S30.43.34,30.43.34'
-(3) [Array(3), Array(5), Array(5)]
-    0: (3) ["M", 18, 69.48]
-    1: (5) ["s", -0.6, -11.27, -3, -30.86]
-    2: (5) ["S", 30.43, 0.34, 30.43, 0.34]
+source: 'M18,69.48s-.6-11.27-3-30.86S30.43.34,30.43.34'
+data: [Array(3), Array(5), Array(5)]
+    0: ["M", 18, 69.48]
+    1: ["s", -0.6, -11.27, -3, -30.86]
+    2: ["S", 30.43, 0.34, 30.43, 0.34]
 */
 
 function SimpleCurve() {
