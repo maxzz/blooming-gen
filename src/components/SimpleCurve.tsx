6a2ca45bd2fb057c36f45caf3fb029e7d320b60d
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-04 06:02:54 -0700

    Feature: WIP parse d path attribute. 15. testing line and control points

--- src/components/SimpleCurve.tsx
+++ src/components/SimpleCurve.tsx
@@ -83,9 +83,9 @@ function MarkPathPoints({ pathPoints, ...rest }: { pathPoints: PathPoint[]; } & 
     return (<>
         {pathPoints.map((point, index) => {
             return <React.Fragment key={index}>
-                {point.d?.map((xy, indexXY) =>
-                    <circle cx={xy.x} cy={xy.y} {...rest} key={`${index}.${indexXY}`}>
-                        <title>{xy.x}, {xy.y}</title>
+                {point.d?.map((xy, indexXY, arr) =>
+                    <circle cx={xy.x} cy={xy.y} {...rest} fill={indexXY === arr.length-1 ? 'tomato': 'black'} key={`${index}.${indexXY}`}>
+                        <title>{point.c}:{index}.{indexXY}: {xy.x}, {xy.y}</title>
                     </circle>
                 )}
             </React.Fragment>;
@@ -95,10 +95,11 @@ function MarkPathPoints({ pathPoints, ...rest }: { pathPoints: PathPoint[]; } & 
 
 function renderPathPoint(path: string): PathPoint[] {
     let tuples = parsePathString(path);
+    console.log('tuples: ', JSON.stringify(tuples));
     tuples = pathToAbsolute(tuples);
+    console.log('abs   : ', JSON.stringify(tuples));
     let pp = pathPointsFromPath(tuples);
-
-    console.log('pp: ', pp);
+    console.log('pps   : ', JSON.stringify(pp));
     return pp;
 }
 
@@ -111,6 +112,8 @@ function SimpleCurve() {
 
     let pp1 = renderPathPoint('M18,69.48S33.7,60,33.7,49s-4.46-16.32-6.24-24.63,3-24,3-24A142.07,142.07,0,0,0,14.11,12.8C7.71,18.56.76,25.27.16,36.84S18,69.48,18,69.48Z');
     let pp2 = renderPathPoint('M18,69.48s-.6-11.27-3-30.86S30.43.34,30.43.34');
+    // let pp2 = renderPathPoint('M10,10l40,40');
+    //console.log('pp2: ', JSON.stringify(pp2, null, 4));
 
     return (
         <div className="pt-8 max-w-md mx-auto bg-indigo-100">
@@ -124,15 +127,18 @@ function SimpleCurve() {
                     <MarkPlaces places={ctrlPlaces} stroke="red"/>
                     <path d={linePath} stroke="black" fill="none" /> */}
 
-                    <MarkPathPoints pathPoints={pp1} stroke="green" />
-                    <MarkPathPoints pathPoints={pp2} />
+                    <MarkPathPoints pathPoints={pp1} />
+                    <MarkPathPoints pathPoints={pp2} fill="red" />
+                    <MarkLines pathPoints={pp1} />
                 </svg>
             </div>
 
             <div className="ml-4 py-2" >
-                <svg className="w-24 max-h-24 bg-indigo-400" viewBox="0 0 33.84 69.68" fill="none" stroke="green">
-                    <MarkPathPoints pathPoints={pp1} stroke="green" />
-                    <MarkPathPoints pathPoints={pp2} />
+                <svg className="w-72 h-72 bg-indigo-400" viewBox="0 0 103.84 139.68" fill="none" stroke="green">
+                {/* <svg className="w-24 max-h-24 bg-indigo-400" viewBox="0 0 33.84 69.68" fill="none" stroke="green"> */}
+                    <MarkGrid x={0} y={0} visible={true} />
+                    <MarkPathPoints pathPoints={pp1} />
+                    <MarkPathPoints pathPoints={pp2} fill="red" />
                     <path d="M18,69.48S33.7,60,33.7,49s-4.46-16.32-6.24-24.63,3-24,3-24A142.07,142.07,0,0,0,14.11,12.8C7.71,18.56.76,25.27.16,36.84S18,69.48,18,69.48Z"/>
                     <path d="M18,69.48s-.6-11.27-3-30.86S30.43.34,30.43.34"/>
                 </svg>                
