f882c81dc48ec4e2a2bfa10a0e80d7acf5b764c5
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-04 04:38:26 -0700

    Feature: WIP parse d path attribute. 14. code clean up

--- src/components/svg-utils.ts
+++ src/components/svg-utils.ts
@@ -8,21 +8,20 @@ export type WH = { // Width and Height
     h: number;
 }
 
-export type PathPoint = {
-    c: PathCmd; //'M' | 'Q'
-    d?: XY[];
-}
-
-type SvgTuple = any[]; //type SvgTuple = [PathCmd, ...number[]];
-
 type PathM = 'M' | 'm';                         // MoveTo
 type PathL = 'L' | 'l' | 'H' | 'h' | 'V' | 'v'; // LineTo
 type PathC = 'C' | 'c' | 'S' | 's';             // Cubic Bézier Curve
 type PathQ = 'Q' | 'q' | 'T' | 't';             // Quadratic Bézier Curve
 type PathA = 'A' | 'a';                         // Elliptical Arc Curve
 type PathZ = 'Z' | 'z';                         // ClosePath
-
 type PathCmd = PathM | PathL | PathC | PathQ | PathA | PathZ;
+
+export type PathPoint = {
+    c: PathCmd; //'M' | 'Q'
+    d?: XY[];
+}
+
+type SvgTuple = any[]; //type SvgTuple = [PathCmd, ...number[]];
 
 const reSpaces = '\x09\x0a\x0b\x0c\x0d\x20\xa0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u2028\u2029';
 const rePathCommand = new RegExp(`([a-z])[${reSpaces},]*((-?\\d*\\.?\\d*(?:e[\\-+]?\\d+)?[${reSpaces}]*,?[${reSpaces}]*)+)`, 'ig');
@@ -82,86 +81,6 @@ export function parsePathString(pathString: string): SvgTuple[] {
     */
 
     return data;
-}
-
-export function pathPointsFromPath(tuples: SvgTuple[]): PathPoint[] {
-    let rv = (tuples || []).map<PathPoint>(tuple => {
-        let c = tuple[0];
-        let xy: XY[] = [];
-        for (let i = 1; i < tuple.length; i+=2) {
-            xy.push({x: tuple[i] as number, y: tuple[i+1] as number || 0});
-        }
-        if (c === 'v' || c === 'V') {
-            xy = [{x: 0, y: xy[0].x}];
-        }
-        return {c, d: xy };
-    });
-    return rv;
-}
-
-/*
-type PathM = 'M' | 'm';                         // MoveTo
-type PathL = 'L' | 'l' | 'H' | 'h' | 'V' | 'v'; // LineTo
-type PathC = 'C' | 'c' | 'S' | 's';             // Cubic Bézier Curve
-type PathQ = 'Q' | 'q' | 'T' | 't';             // Quadratic Bézier Curve
-type PathA = 'A' | 'a';                         // Elliptical Arc Curve
-type PathZ = 'Z' | 'z';                         // ClosePath
-*/
-
-const lowerCase = String.prototype.toLowerCase;
-
-function getPrevPos(point: PathPoint, prevPos: XY): XY {
-    if (!point.d) {
-        return prevPos;
-    }
-    if (!prevPos) {
-        prevPos = {x:0, y:0};
-    }
-    if (point.c != lowerCase.call(point.c)) {
-        switch (lowerCase.call(point.c)) {
-            case 'm':
-            case 'l': return {x: point.d[0].x + prevPos.x, y: point.d[0].y + prevPos.y};
-            case 'h': return {x: point.d[0].x + prevPos.x, y: point.d[0].y + prevPos.y};
-        }
-    } else {
-        switch (point.c) {
-            case 'M':
-            case 'L': return point.d[0];
-            case 'H': return {x: point.d[0].x, y: prevPos.y};
-        }
-    }
-    return prevPos;
-}
-
-function relativeToAbsPos(pathPoints: PathPoint[]): PathPoint[] {
-    let prevPos: XY | undefined;
-    return pathPoints.map((point) => {
-
-        let thisPos: XY | undefined;
-        if (point.d) {
-            if (point.c === 'M') {
-                thisPos = point.d[0];
-            } else if (point.c === 'm') {
-                thisPos = prevPos ? {x: prevPos.x + point.d[0].x, y: prevPos.y + point.d[0].y} : point.d[0];
-            } else if (point.c === 'Q') {
-                thisPos = point.d[1];
-            } else if (point.c === 'q') {
-                thisPos = prevPos ? {x: prevPos.x + point.d[1].x, y: prevPos.y + point.d[1].y} : point.d[1];
-            }
-        }
-
-        //TODO: set initial {x:0, y:0}
-        //case on point.c
-        //separete function give prev and get current
-
-        if (point.c > 'Z' && prevPos) { // i.e. lowercase command
-            //let prevPos = 
-        }
-
-        prevPos = thisPos;
-        
-        return {c: point.c};
-    });
 }
 
 export function pathToAbsolute(pathArray: SvgTuple[]): SvgTuple[] {
@@ -238,4 +157,19 @@ export function pathToAbsolute(pathArray: SvgTuple[]): SvgTuple[] {
         }
     }
     return res;
+}
+
+export function pathPointsFromPath(tuples: SvgTuple[]): PathPoint[] {
+    let rv = (tuples || []).map<PathPoint>(tuple => {
+        let c = tuple[0];
+        let xy: XY[] = [];
+        for (let i = 1; i < tuple.length; i+=2) {
+            xy.push({x: tuple[i] as number, y: tuple[i+1] as number || 0});
+        }
+        if (c === 'v' || c === 'V') {
+            xy = [{x: 0, y: xy[0].x}];
+        }
+        return {c, d: xy };
+    });
+    return rv;
 }
