643975d9cc0ba15cad545dadd29d21478b1dd80e
Max Zakharzhevskiy <maxzz@msn.com>
2021-04-06 20:28:05 -0700

    Feature: add backtracking to path parse. WIP

--- src/components/svg-utils.ts
+++ src/components/svg-utils.ts
@@ -59,79 +59,100 @@ export function parsePathString(pathString: string): SvgTuple[] {
     let paramCounts = {a: 7, c: 6, o: 2, h: 1, l: 2, m: 2, r: 4, q: 4, s: 4, t: 2, v: 1, u: 3, z: 0};
     let data: any[] = [];
 
-    pathString.replace(rePathCommand, function (a: string, b: string, c: string, d: string, ofs: number, ...rest: any[]) {
-        console.log(`-------------------\nofs: ${ofs}\na: ${a}\nb: ${b}\nd: ${d}`, JSON.stringify(rest));
+    pathString.replace(rePathCommand, function (a: string, svgCmd: string, svgCmdParams: string, d: string, ofs: number, ...rest: any[]) {
+        console.log(`-------------------\na: '${a}'\nb: '${svgCmd}'\nc: '${svgCmdParams}'\nd: '${d}'\nofs: ${ofs}\nrest: '${JSON.stringify(rest)}'`);
 
         /*
+            pathString.replace(rePathCommand, function (a: string, b: string, c: string, d: string, ofs: number, ...rest: any[]) {
+                console.log(`-------------------\nofs: ${ofs}\na: ${a}\nb: ${b}\nd: ${d}\nrest: ${JSON.stringify(rest)}`); ...
+
             -------------------
+            a: 'M20,20    '
+            b: 'M'
+            c: '20,20    '
+            d: '20    '
             ofs: 0
-            a: M20,20    
-            b: M
-            d: 20     ["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]
+            rest: '["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]'
             -------------------
+            a: 'Q40,20 40,100    '
+            b: 'Q'
+            c: '40,20 40,100    '
+            d: '100    '
             ofs: 10
-            a: Q40,20 40,100    
-            b: Q
-            d: 100     ["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]
+            rest: '["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]'
             -------------------
+            a: 'Q70,20 80,90    '
+            b: 'Q'
+            c: '70,20 80,90    '
+            d: '90    '
             ofs: 27
-            a: Q70,20 80,90    
-            b: Q
-            d: 90     ["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]
+            rest: '["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]'
             -------------------
+            a: 'T 100,100    '
+            b: 'T'
+            c: '100,100    '
+            d: '100    '
             ofs: 43
-            a: T 100,100    
-            b: T
-            d: 100     ["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]
+            rest: '["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]'
             -------------------
+            a: 'T 120,120    '
+            b: 'T'
+            c: '120,120    '
+            d: '120    '
             ofs: 56
-            a: T 120,120    
-            b: T
-            d: 120     ["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]
+            rest: '["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]'
             -------------------
+            a: 'T140,100    '
+            b: 'T'
+            c: '140,100    '
+            d: '100    '
             ofs: 69
-            a: T140,100    
-            b: T
-            d: 100     ["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]
+            rest: '["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]'
             -------------------
+            a: 'T150,100    '
+            b: 'T'
+            c: '150,100    '
+            d: '100    '
             ofs: 81
-            a: T150,100    
-            b: T
-            d: 100     ["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]
+            rest: '["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]'
             -------------------
+            a: 'T160,100    '
+            b: 'T'
+            c: '160,100    '
+            d: '100    '
             ofs: 93
-            a: T160,100    
-            b: T
-            d: 100     ["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]
+            rest: '["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]'
             -------------------
+            a: 'T170,100'
+            b: 'T'
+            c: '170,100'
+            d: '100'
             ofs: 105
-            a: T170,100
-            b: T
-            d: 100 ["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]        
+            rest: '["M20,20    Q40,20 40,100    Q70,20 80,90    T 100,100    T 120,120    T140,100    T150,100    T160,100    T170,100"]'
         */
 
         let params: any[] = [];
-        let name = b.toLowerCase() as keyof typeof paramCounts;
+        let name = svgCmd.toLowerCase() as keyof typeof paramCounts;
 
-        c.replace(rePathValues, function (a: string, b: string) {
+        svgCmdParams.replace(rePathValues, function (a: string, b: string) {
             b && params.push(+b);
         } as any);
 
         if (name == "m" && params.length > 2) {
-            data.push([b].concat(params.splice(0, 2)));
+            data.push([svgCmd].concat(params.splice(0, 2)));
             name = "l";
-            b = b == "m" ? "l" : "L";
+            svgCmd = svgCmd == "m" ? "l" : "L";
         }
 
         if (name == "o" && params.length == 1) {
-            data.push([b, params[0]]);
+            data.push([svgCmd, params[0]]);
         }
 
         if (name == "r") {
-            data.push([b].concat(params));
+            data.push([svgCmd].concat(params));
         } else {
             while (params.length >= paramCounts[name]) {
-                data.push([b].concat(params.splice(0, paramCounts[name])));
+                data.push([svgCmd].concat(params.splice(0, paramCounts[name])));
                 if (!paramCounts[name]) {
                     break;
                 }
